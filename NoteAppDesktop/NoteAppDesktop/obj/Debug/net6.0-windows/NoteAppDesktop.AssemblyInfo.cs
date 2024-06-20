40ad40d417ec233103ca6f437b7a07c0eabafbfa
Holzi69 <115150336+Holzi69@users.noreply.github.com>
2024-06-20 14:01:37 +0200

    Vergessene Bilder 2.0

--- NoteAppDesktop/NoteAppDesktop/obj/Debug/net6.0-windows/NoteAppDesktop.AssemblyInfo.cs
+++ NoteAppDesktop/NoteAppDesktop/obj/Debug/net6.0-windows/NoteAppDesktop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoteAppDesktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce1ef7f75d41c73480b27a6c313eaca3369b7e80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5217dd165fcb238da33aa038e21b9414775ed88e")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoteAppDesktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoteAppDesktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
